89727a9d9d0886dcdf73e91fa75c37757b875c84
JUNHO0712 <jeff070@naver.com>
2026-03-24 18:33:11 +0900

    Delete trip-app/trip-front directory

--- trip-app/trip-front/src/hooks/useKakaoLoader.ts
+++
@@ -1,8 +0,0 @@
-import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
-
-export default function useKakaoLoader() {
-  useKakaoLoaderOrigin({
-    appkey: import.meta.env.VITE_KAKAO_API,
-    libraries: ['clusterer', 'drawing', 'services'],
-  });
-}
