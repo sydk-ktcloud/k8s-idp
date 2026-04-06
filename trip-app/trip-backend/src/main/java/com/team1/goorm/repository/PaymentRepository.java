37e4657106d3675aa10c1f6a1428579717f1de94
JUNHO0712 <jeff070@naver.com>
2026-04-06 23:03:57 +0900

    Delete trip-app/trip-backend directory

--- trip-app/trip-backend/src/main/java/com/team1/goorm/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team1.goorm.repository;
-
-import com.team1.goorm.domain.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
--- trip-app/trip-backend/src/main/java/com/team1/goorm/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team1.goorm.repository;
-
-import com.team1.goorm.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
